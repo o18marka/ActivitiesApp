9cc58dccd5465727136da3d0f7732b2a84c67866
Marcus Brohede <a97marbr@gmail.com>
2018-03-19 08:54:36 +0100

    Initial commit

---
+++ app/src/main/java/com/example/brom/activitiesapp/MainActivity.java
@@ -0,0 +1,37 @@
+package com.example.brom.activitiesapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+    private String[] mountainNames = {"Matterhorn","Mont Blanc","Denali"};
+    private String[] mountainLocations = {"Alps","Alps","Alaska"};
+    private int[] mountainHeights ={4478,4808,6190};
+    // Create ArrayLists from the raw data above and use these lists when populating your ListView.
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        // 1. Create a ListView as in previous assignment
+        // 2. Create a new activity named "MountainDetailsActivity
+        // 3. Create a new Layout file for the MountainDetailsActivity called
+        //    "activity_mountaindetails". MountainDetailsActivity must have MainActivity as its
+        //    ´parent activity.
+        // 4. The layout file created in step 3 must have a LinearLayout
+        // 5. The layout file created in step 3 must be able to display
+        //    * Mountain Name
+        //    * Mountain Location
+        //    * Mountain Height
+        // 6. When tapping on a list item: create an Intent that includes
+        //    * Mountain Name
+        //    * Mountain Location
+        //    * Mountain Height
+        // 7. Display the MountainDetailsActivity with the data from the Intent created in step
+        //    6
+        // 8. From the MountainDetailsActivity you should have an option to "go back" using an
+        //    left arro button. This is done by letting the MainActivity be the parent activity to
+        //    MountainDetailsActivity.
+    }
+}
